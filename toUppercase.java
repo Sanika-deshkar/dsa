b72ca39306a0745cfe27f68d6b5b134a8909f6d1
Sanika-deshkar <sdeshkar2005@gmail.com>
2026-01-27 18:45:38 +0530

    To convert 1st letter of every word in a string to uppercas

---
+++ toUppercase.java
@@ -0,0 +1,22 @@
+public class UpperCase{
+    public static String ToUpperCase(String str){
+        StringBuilder s = new StringBuilder("");
+        char ch = Character.toUpperCase(str.charAt(0));
+        s.append(ch);
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i)==' ' && i<str.length()-1){
+                s.append(str.charAt(i));
+                i++;
+                s.append(Character.toUpperCase(str.charAt((i))));
+            }
+            else{
+                s.append(str.charAt(i));
+            }
+        }
+        return s.toString();
+    }
+    public static void main(String[] args) {
+        String str = "hi i am sanika";
+        System.out.println(ToUpperCase(str));
+    }
+}
