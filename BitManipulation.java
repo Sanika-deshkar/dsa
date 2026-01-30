ef517a79aebe6749db796c3e159f6370e1724d00
Sanika-deshkar <sdeshkar2005@gmail.com>
2026-01-30 18:53:37 +0530

    Refactor bit manipulation methods and add updatebit

--- BitManipulation.java
+++ BitManipulation.java
@@ -1,6 +1,6 @@
 public class BitManipulation{
     public static int getbit(int n,int i){
-        int bitMask = 1<<i; //left shift
+        int bitMask = 1<<i;
         if((n&bitMask)==0){
             return 0;
         }else{
@@ -12,13 +12,22 @@ public class BitManipulation{
         return n|bitMask;
     }
     public static int clearbit(int n,int i){
-        int bitMask = ~(1<<i); //NOT of leftshift
+        int bitMask = ~(1<<i);
         return n&bitMask;
+    }
+    public static int updatebit(int n, int i,int newBit){
+        if(newBit==0){
+            return clearbit(n, i);
+        }
+        else{
+            return setbit(n, i);
+        }
     }
     public static void main(String[] args) {
         System.out.println((int)getbit(10,3));
         System.out.println((int)setbit(10, 2));
         System.out.println((int)clearbit(10, 1));
+        System.out.println((int)updatebit(10, 1, 2));
 
     }
 }
